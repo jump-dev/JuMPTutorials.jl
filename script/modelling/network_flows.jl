1e49e6c7d8bda818e60ff8cb6ccbfb028a966964
Arpit Bhatia <arpit16229@iiitd.ac.in>
2019-06-15 11:23:42 +0530

    Style fixes for Network Flows

--- script/modelling/network_flows.jl
+++ script/modelling/network_flows.jl
@@ -43,10 +43,10 @@ n = size(G)[1]
 model = Model(with_optimizer(GLPK.Optimizer))
 
 @variable(model, x[1:n,1:n], Bin)
-@constraint(model, con[i = 1:n; i!=1 && i!=2], sum(x[i,:]) == sum(x[:,i]))
-@constraint(model, con2[i = 1:n, j=1:n; G[i,j]==0], x[i,j] == 0)
-@constraint(model, sum(x[1,:]) - sum(x[:,1]) == 1)
-@constraint(model, sum(x[2,:]) - sum(x[:,2]) == -1)
+@constraint(model, [i = 1:n, j = 1:n; G[i,j] == 0], x[i,j] == 0) # Arcs with zero cost are not a part of the path as they do no exist
+@constraint(model, [i = 1:n; i != 1 && i != 2], sum(x[i,:]) == sum(x[:,i])) # Flow conservation constraint
+@constraint(model, sum(x[1,:]) - sum(x[:,1]) == 1) # Flow coming out of source = 1
+@constraint(model, sum(x[2,:]) - sum(x[:,2]) == -1) # Flowing coming out of destination = -1 i.e. Flow entering destination = 1  
 @objective(model, Min, sum(G .* x))
 
 optimize!(model)
@@ -81,8 +81,8 @@ n = size(G)[1]
 
 model = Model(with_optimizer(GLPK.Optimizer))
 @variable(model, x[1:n,1:n], Bin)
-@constraint(model, [i=1:n], sum(x[:,i]) == 1)
-@constraint(model, [j=1:n], sum(x[j,:]) == 1)
+@constraint(model, [i = 1:n], sum(x[:,i]) == 1) # One person can only be assigned to one object
+@constraint(model, [j = 1:n], sum(x[j,:]) == 1) # One object can only be assigned to one person
 @objective(model, Max, sum(G .* x))
 
 optimize!(model)
@@ -117,9 +117,9 @@ n = size(G)[1]
 
 model = Model(with_optimizer(GLPK.Optimizer))
 
-@variable(model, f[1:n,1:n] >=0)
-@constraint(model, capacity[i = 1:n, j=1:n], f[i,j] <= G[i,j])
-@constraint(model, flow[i = 1:n; i!=1 && i!=8], sum(f[i,:]) == sum(f[:,i]))
+@variable(model, f[1:n,1:n] >= 0)
+@constraint(model, [i = 1:n, j = 1:n], f[i,j] <= G[i,j]) # Capacity constraints
+@constraint(model, [i = 1:n; i != 1 && i != 8], sum(f[i,:]) == sum(f[:,i])) # Flow conservation contraints
 @objective(model, Max, sum(f[1, :]))
 
 optimize!(model)
