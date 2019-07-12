1fdba8c828bb7bde99bb009a1e3aaec41165a572
Arpit Bhatia <arpit16229@iiitd.ac.in>
2019-07-12 18:07:09 +0530

    Change Weave functions (Fixes #20)

--- src/JuMPTutorials.jl
+++ src/JuMPTutorials.jl
@@ -5,30 +5,18 @@ using Weave
 repo_directory = joinpath(@__DIR__,"..")
 
 function weave_file(folder,file)
+    cd(joinpath(repo_directory,"script",folder))
+    
     filename = split(file, ".")[1]
     srcpath = joinpath(repo_directory, "script", folder, file)
     testpath = joinpath(repo_directory, "test", folder, file)
-    notebookpath = joinpath(repo_directory, "notebook", folder, string(filename, ".ipynb"))
+    notebookpath = joinpath(repo_directory, "notebook", folder)
 
     if mtime(srcpath) > mtime(testpath) || mtime(testpath)==0
         @warn "Updating tests for $filename as it has been updated since the last weave."
         tangle(srcpath, out_path=testpath)
     else
         @warn "Skipping tests for $filename as it has not been updated."
-    end
-
-    if mtime(srcpath) > mtime(htmlpath) || mtime(htmlpath)==0
-        @warn "Weaving $filename to HTML as it has been updated since the last weave."
-        weave(srcpath, doctype="md2html")
-    else
-        @warn "Skipping HTML for $filename as it has not been updated."
-    end
-
-    if mtime(srcpath) > mtime(pdfpath) || mtime(pdfpath)==0
-        @warn "Weaving $filename to PDF as it has been updated since the last weave."
-        weave(srcpath, doctype="md2pdf")
-    else
-        @warn "Skipping PDF for $filename as it has not been updated."
     end
 
     if mtime(srcpath) > mtime(notebookpath) || mtime(notebookpath)==0
@@ -38,6 +26,7 @@ function weave_file(folder,file)
         @warn "Skipping Jupyter Notebook for $filename as it has not been updated."
     end
 
+    cd(joinpath(repo_directory,"src"))
 end
 
 function weave_all()
@@ -47,12 +36,46 @@ function weave_all()
 end
 
 function weave_folder(folder)
+
     for file in readdir(joinpath(repo_directory,"script",folder))
+        println("")
         println("Building $(joinpath(folder,file))")
         try
             weave_file(folder,file)
         catch
         end
+        println("")
+    end
+end
+
+function weave_file_f(folder,file)
+    cd(joinpath(repo_directory,"script",folder))
+    
+    srcpath = joinpath(repo_directory, "script", folder, file)
+    testpath = joinpath(repo_directory, "test", folder, file)
+    notebookpath = joinpath(repo_directory, "notebook", folder)
+
+    tangle(srcpath, out_path = testpath)
+    notebook(srcpath, notebookpath)
+
+    cd(joinpath(repo_directory,"src"))
+end
+
+function weave_all_f()
+    for folder in readdir(joinpath(repo_directory,"script"))
+        weave_folder_f(folder)
+    end
+end
+
+function weave_folder_f(folder)
+    for file in readdir(joinpath(repo_directory,"script",folder))
+        println("")
+        println("Building $(joinpath(folder,file))")
+        try
+            weave_file_f(folder,file)
+        catch
+        end
+        println("")
     end
 end
 
