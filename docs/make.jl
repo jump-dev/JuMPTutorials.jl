7c877f42eed83eef3bfe12cbc12ae8bc9d99259d
Arpit Bhatia <arpit16229@iiitd.ac.in>
2019-07-18 14:30:07 +0530

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,10 @@ makedocs(
     authors  = "Arpit Bhatia",
     pages = [
         "Home" => "index.md",
-        "API" => "api.md"
+        "Adding a New Tutorial" => "new.md",
+        "Notes" => "notes.md",
+        "Future Work" => "future.md",
+        "Function Index" => "api.md"
     ]
 )
 
