14b851ad456785dc4125340ff31c09c86aa9517e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-07-18 02:44:48 +0000

    build based on 1b909ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#JuMPTutorials.jl-1","page":"Home","title":"JuMPTutorials.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [JuMPTutorials]","category":"page"}]
+}
