335c38c3e6b1dc93fa6f1fbac046eab569b298b7
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-04-01 18:56:43 -0400

    Add a Facility Location tutorial

---
+++ test/modelling/facility_location.jl
@@ -0,0 +1,176 @@
+
+n = 2  # number of facility locations
+m = 5  # number of clients
+
+
+# Draw costs
+Random.seed!(0)
+f = rand(1000:10000, n);
+c = rand(0:1000, m, n);
+
+
+using JuMP
+using GLPK
+using Cbc
+
+# Create a model
+ufl = Model();
+
+
+# Create our variables
+@variable(ufl, y[1:m], Bin);
+@variable(ufl, x[1:m, 1:n], Bin);
+
+
+# Add constraints
+
+# Each client is serve exactly once
+@constraint(
+    ufl,
+    [i in 1:m],
+    sum(x[i, j] for j in 1:n) == 1
+);
+
+# Fixed cost of opening facilities
+@constraint(
+    ufl,
+    [i in 1:m, j in 1:n],
+    x[i, j] <= y[j]
+);
+
+
+# Set objective
+F = sum([f[j]*y[j] for j in 1:n]);
+C = sum([c[i, j]*x[i, j] for i in 1:m for j in 1:n]);
+                
+@objective(ufl, Min, F + C);
+
+
+# Set optimizer
+set_optimizer(ufl, with_optimizer(GLPK.Optimizer))
+
+
+# Solve the uncapacitated facility location problem with GLPK
+optimize!(ufl)
+println("Optimal value: ", objective_value(ufl))
+
+
+# Get y and x solutions
+xsol = value.(x);
+println("Optimal solution x: ", value.(x))
+ysol = value.(y);
+println("Optimal solution y: ", value.(y))
+
+
+# relax all binary variables
+for var in x
+    is_binary(var) && unset_binary(var)
+    set_lower_bound(var, 0.0)
+    set_upper_bound(var, 1.0)
+end
+
+for var in y
+    is_binary(var) && unset_binary(var)
+    set_lower_bound(var, 0.0)
+    set_upper_bound(var, 1.0)
+end
+
+
+# Solve the LP relaxation
+optimize!(ufl)
+lp_val = objective_value(ufl)
+println("Optimal value of relaxed ufl: ", lp_val)
+
+
+# Get y and x solutions
+lp_ysol = value.(y);
+println("Optimal solution y: ", value.(y))
+lp_xsol = value.(x);
+println("Optimal solution x: ", value.(x))
+
+
+# Set all variables to be binary
+for var in x
+    set_binary(var)
+end
+
+for var in y
+    set_binary(var)
+end
+
+optimize!(ufl)
+mip_val = objective_value(ufl)
+println("Optimal value of integer ufl: ", mip_val)
+
+
+# Integrality gap
+(mip_val - lp_val) / mip_val
+
+
+n = 10  # number of facility locations
+m = 30  # number of clients
+
+# Draw costs
+Random.seed!(0)
+f = rand(1000:10000, n);
+c = rand(0:1000, m, n);
+
+# Clients' demands
+a = rand(1:10, m);
+
+# Capacities
+q = rand(30:40, n);
+
+
+# Instantiate an empty model
+cfl = Model();
+
+
+# Create variables
+y = @variable(cfl, y[1:n], Bin);
+x = @variable(cfl, x[1:m, 1:n], Bin);
+
+
+# set objective
+C = sum([c[i, j]*x[i, j] for i in 1:m for j in 1:n])  # demand serving cost
+F = sum([f[j]*y[j] for j in 1:n])  # fixed cost
+
+@objective(cfl, Min, C + F);
+
+
+# Add constraints
+
+# Each client is serve exactly once
+ctr_ = @constraint(
+    cfl,                             # add constraints to model
+    [i in 1:m],                      # there are `m` constraints, indexed by `i`
+    sum(x[i, j] for j in 1:n) == 1   # the actual constraint
+);
+
+# Capacity constraints
+ctr_capacity = @constraint(
+    cfl,
+    [j in 1:n],
+    sum(a[i] * x[i, j] for i in 1:m) <= q[j]*y[j]
+);
+
+
+# Set optimizer
+set_optimizer(
+    cfl,
+    with_optimizer(
+        GLPK.Optimizer,
+        msg_lev=3,    # verbosity level
+        tm_lim=10000  # time limit, in ms
+    )
+)
+
+
+# Best solution found so far
+println("Optimal value: ", objective_value(cfl))
+
+
+# Solve the capacitated facility location problem with Cbc
+set_optimizer(cfl, with_optimizer(Cbc.Optimizer))
+optimize!(cfl)
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -35,81 +35,168 @@ cd("..")
 
 cd("modelling")
 @testset "Modelling Examples" begin
-    include("modelling/n-queens.jl")
-    @test sum([sum(value.(x)[i, :]) == 1 for i=1:N]) == N
-    @test sum([sum(value.(x)[:, i]) == 1 for i=1:N]) == N
-    @test sum([sum(diag(value.(x),i)) <= 1 for i in -(N-1):(N-1)]) == length(-(N-1):(N-1))
-    @test sum([sum(diag(reverse(value.(x),dims=1), i)) <=1 for i in -(N-1):(N-1)]) == length(-(N-1):(N-1))
-    @test JuMP.termination_status(model) == MOI.OPTIMAL
 
-    include("modelling/sudoku.jl")
-    @test sol == [
-        5  3  4  6  7  8  9  1  2;
-        6  7  2  1  9  5  3  4  8;
-        1  9  8  3  4  2  5  6  7;
-        8  5  9  7  6  1  4  2  3;
-        4  2  6  8  5  3  7  9  1;
-        7  1  3  9  2  4  8  5  6;
-        9  6  1  5  3  7  2  8  4;
-        2  8  7  4  1  9  6  3  5;
-        3  4  5  2  8  6  1  7  9]
+include("modelling/n-queens.jl")
+@test sum([sum(value.(x)[i, :]) == 1 for i=1:N]) == N
+@test sum([sum(value.(x)[:, i]) == 1 for i=1:N]) == N
+@test sum([sum(diag(value.(x),i)) <= 1 for i in -(N-1):(N-1)]) == length(-(N-1):(N-1))
+@test sum([sum(diag(reverse(value.(x),dims=1), i)) <=1 for i in -(N-1):(N-1)]) == length(-(N-1):(N-1))
+@test JuMP.termination_status(model) == MOI.OPTIMAL
 
-    include("modelling/problems_on_graphs.jl")
-    @test value.(y) == [0.0, 1.0, 1.0, 1.0, 0.0, 0.0]
-    @test value.(x) == [0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
-    @test value.(m) == [
-        0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
-        0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0;
-        0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
-        0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
-        1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
-        0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
-        0.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0;
-        0.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0
-    ]
-    @test value(sum(z)) ≈ 3
-    @test sum(c) ≈ nv(g)
+include("modelling/sudoku.jl")
+@test sol == [
+    5  3  4  6  7  8  9  1  2;
+    6  7  2  1  9  5  3  4  8;
+    1  9  8  3  4  2  5  6  7;
+    8  5  9  7  6  1  4  2  3;
+    4  2  6  8  5  3  7  9  1;
+    7  1  3  9  2  4  8  5  6;
+    9  6  1  5  3  7  2  8  4;
+    2  8  7  4  1  9  6  3  5;
+    3  4  5  2  8  6  1  7  9]
 
-    include("modelling/network_flows.jl")
-    @test objective_value(shortest_path) == 55.0
-    @test value.(x) == [
-        0.0 0.0 1.0 0.0 0.0;
-        0.0 0.0 0.0 0.0 0.0;
-        0.0 0.0 0.0 1.0 0.0;
-        0.0 1.0 0.0 0.0 0.0;
-        0.0 0.0 0.0 0.0 0.0]
-    @test objective_value(assignment) ≈ 20.0
-    @test value.(y) == [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 1.0 0.0 0.0 0.0; 0.0 0.0 0.0 1.0]
-    @test objective_value(max_flow) ≈ 6.0
+include("modelling/problems_on_graphs.jl")
+@test value.(y) == [0.0, 1.0, 1.0, 1.0, 0.0, 0.0]
+@test value.(x) == [0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+@test value.(m) == [
+    0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+    1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+    0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
+    0.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0
+]
+@test value(sum(z)) ≈ 3
+@test sum(c) ≈ nv(g)
 
-    include("modelling/finance.jl")
-    @test objective_value(financing) ≈ 92.49694915254233
-    @test objective_value(auction) ≈ 21.0
-    @test value.(y) == [1.0, 1.0, 1.0, 0.0, 0.0, 0.0]
-    @test objective_value(portfolio) ≈ 22634.41784988414
-    @test value.(x) ≈ [497.0455298498642, 0.0, 502.95448015948074]
-    include("modelling/power_systems.jl")
-    @test g_opt == [1000.0, 300.0]
-    @test w_opt == 200.0
-    @test w_f - w_opt == 0
-    @test obj == 90000.0
-    include("modelling/geometric_problems.jl")
-    @test value.(p) ≈ [
-        0.44790964261631827    0.0468981793661497;
-        -0.03193526635198919   -0.6706136210384356;
-        0.404335805799056     -0.45130815913688466;
-        -0.39429534726904925   -0.13282535401213758;
-        0.02532703978422118    0.4124207687120701;
-        -0.0016520566419420052 -0.43954821308159137;
-        1.0         1.0;
-        1.0        -1.0;
-        -1.0        -1.0;
-        -1.0         1.0;
-        1.0        -0.5;
-        -1.0        -0.2;
-        -0.2        -1.0;
-        0.1         1.0]
-    include("modelling/rocket_control.jl")
-    @test objective_value(rocket) ≈ 1.0128340648308058
+include("modelling/network_flows.jl")
+@test objective_value(shortest_path) == 55.0
+@test value.(x) == [
+    0.0 0.0 1.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 1.0 0.0;
+    0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0]
+@test objective_value(assignment) ≈ 20.0
+@test value.(y) == [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 1.0 0.0 0.0 0.0; 0.0 0.0 0.0 1.0]
+@test objective_value(max_flow) ≈ 6.0
+
+include("modelling/finance.jl")
+@test objective_value(financing) ≈ 92.49694915254233
+@test objective_value(auction) ≈ 21.0
+@test value.(y) == [1.0, 1.0, 1.0, 0.0, 0.0, 0.0]
+@test objective_value(portfolio) ≈ 22634.41784988414
+@test value.(x) ≈ [497.0455298498642, 0.0, 502.95448015948074]
+include("modelling/power_systems.jl")
+@test g_opt == [1000.0, 300.0]
+@test w_opt == 200.0
+@test w_f - w_opt == 0
+@test obj == 90000.0
+include("modelling/geometric_problems.jl")
+@test value.(p) ≈ [
+    0.44790964261631827    0.0468981793661497;
+    -0.03193526635198919   -0.6706136210384356;
+    0.404335805799056     -0.45130815913688466;
+    -0.39429534726904925   -0.13282535401213758;
+    0.02532703978422118    0.4124207687120701;
+    -0.0016520566419420052 -0.43954821308159137;
+    1.0         1.0;
+    1.0        -1.0;
+    -1.0        -1.0;
+    -1.0         1.0;
+    1.0        -0.5;
+    -1.0        -0.2;
+    -0.2        -1.0;
+    0.1         1.0]
+include("modelling/rocket_control.jl")
+@test objective_value(rocket) ≈ 1.0128340648308058
+
+include("modelling/sudoku.jl")
+@test sol == [
+    5  3  4  6  7  8  9  1  2;
+    6  7  2  1  9  5  3  4  8;
+    1  9  8  3  4  2  5  6  7;
+    8  5  9  7  6  1  4  2  3;
+    4  2  6  8  5  3  7  9  1;
+    7  1  3  9  2  4  8  5  6;
+    9  6  1  5  3  7  2  8  4;
+    2  8  7  4  1  9  6  3  5;
+    3  4  5  2  8  6  1  7  9]
+include("modelling/problems_on_graphs.jl")
+@test value.(y) == [0.0, 1.0, 1.0, 1.0, 0.0, 0.0]
+@test value.(x) == [0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+@test value.(m) == [
+    0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+    1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+    0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0;
+    0.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0]
+@test value.(z) == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0]
+@test c == [
+    0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0;
+    0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0]
+include("modelling/network_flows.jl")
+@test objective_value(shortest_path) == 55.0
+@test value.(x) == [
+    0.0 0.0 1.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 1.0 0.0; 
+    0.0 1.0 0.0 0.0 0.0;
+    0.0 0.0 0.0 0.0 0.0]
+@test objective_value(assignment) == 20.0
+@test value.(y) == [0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 1.0 0.0 0.0 0.0; 0.0 0.0 0.0 1.0]
+@test objective_value(max_flow) == 6.0
+@test value.(f) == [
+    0.0 3.0 2.0 1.0 0.0 0.0 0.0 0.0; 
+    0.0 0.0 0.0 0.0 3.0 0.0 0.0 0.0; 
+    0.0 0.0 0.0 0.0 1.0 0.0 1.0 0.0; 
+    0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0; 
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 4.0; 
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0; 
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0; 
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
+include("modelling/finance.jl")
+@test objective_value(financing) ≈ 92.49694915254233
+@test objective_value(auction) == 21.0
+@test value.(y) == [1.0, 1.0, 1.0, 0.0, 0.0, 0.0]
+@test objective_value(portfolio) ≈ 22634.41784988414
+@test value.(x) ≈ [497.0455298498642, 0.0, 502.95448015948074]
+include("modelling/power_systems.jl")
+@test g_opt == [1000.0, 300.0]
+@test w_opt == 200.0
+@test w_f - w_opt == 0
+@test obj == 90000.0
+include("modelling/geometric_problems.jl")
+@test value.(p) ≈ [
+     0.44790964261631827    0.0468981793661497; 
+    -0.03193526635198919   -0.6706136210384356; 
+     0.404335805799056     -0.45130815913688466; 
+    -0.39429534726904925   -0.13282535401213758; 
+     0.02532703978422118    0.4124207687120701; 
+    -0.0016520566419420052 -0.43954821308159137; 
+     1.0         1.0; 
+     1.0        -1.0; 
+    -1.0        -1.0; 
+    -1.0         1.0; 
+     1.0        -0.5; 
+    -1.0        -0.2; 
+    -0.2        -1.0; 
+     0.1         1.0]
+include("modelling/rocket_control.jl")
+@test objective_value(rocket) ≈ 1.0128340648308058
+include("modelling/facility_location.jl")
 end
 cd("..")
